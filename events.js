da53522c0c5c9b6d3c3bfc82f8034f0e9a0e7e00
jakelaz <jacobmlazarus@gmail.com>
2025-03-30 11:19:16 -0400

    change event titles

--- events.js
+++ events.js
@@ -190,7 +190,7 @@ const events = [
     location_address: "28 W 23rd St, New York, NY",
     neighborhood: "Flatiron",
     personalized_title: "Sweat session that delivers...",
-    calendar_title: "Sweat session that delivers..."
+    calendar_title: "This is an event title..."
   },
   {
     event_id: "evt_015",
